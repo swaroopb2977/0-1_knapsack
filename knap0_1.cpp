29a58b3fceb2651b9c83a871d6763602627ee5ab
Swaroop Bhanja <swaroopbhanja996@gmail.com>
2022-06-22 16:59:43 +0530

    added Ex:-1st knapsack soln

--- knap0_1.cpp
+++ knap0_1.cpp
@@ -32,7 +32,7 @@ int knapsack(int wt[], int val[],int w,int n)
 
 
 // Memorizartion
-
+/*
 int knapsack(int wt[], int val[],int w,int n,int dp[8][4])
 {
     if(w==0||n==0)
@@ -57,14 +57,46 @@ int knapsack(int wt[], int val[],int w,int n,int dp[8][4])
         return dp[n][w];
     }
 }
+*/
+
 
 
 int main()
 {
     int wt[]={1,3,4,5};
     int val[]={1,4,5,7};
-    int dp[8][4];
+    int dp[5][9];
     memset(dp,-1,sizeof(dp));
-    cout<<knapsack(wt,val,8,4,dp);
+    // Dynamic soln:-
+
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            if(j==0||i==0)
+            {
+                dp[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=1;i<5;i++)
+    {
+        for(int j=1;j<9;j++)
+        {
+            if(wt[i-1]<=j)
+            {
+                dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+            }
+            else
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+
+    cout<<dp[4][8];
+
+   // cout<<knapsack(wt,val,8,4,dp);
 
 }

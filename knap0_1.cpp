78c68a516e06ee8febb6dea8a0709dd303b4e398
Swaroop Bhanja <swaroopbhanja996@gmail.com>
2022-06-22 13:38:04 +0530

    added memorization for knapsack

--- knap0_1.cpp
+++ knap0_1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 // Recursive solution
-
+/*
 int knapsack(int wt[], int val[],int w,int n)
 {
     if(n==0||w==0)
@@ -28,13 +28,43 @@ int knapsack(int wt[], int val[],int w,int n)
         return knapsack(wt,val,w,n-1);
     }
 }
+*/
 
+
+// Memorizartion
+
+int knapsack(int wt[], int val[],int w,int n,int dp[8][4])
+{
+    if(w==0||n==0)
+    {
+        return 0;
+    }
+
+    if(dp[n][w]!=-1)
+    {
+        return dp[n][w];
+    }
+
+    else if (wt[n-1]<=w)
+    {
+         dp[n][w]= max(val[n-1]+knapsack(wt,val,w-wt[n-1],n-1,dp),knapsack(wt,val,w,n-1,dp));
+          return dp[n][w];
+    }
+
+    else if(wt[n-1]>w)
+    {
+        dp[n][w]= knapsack(wt,val,w,n-1,dp);
+        return dp[n][w];
+    }
+}
 
 
 int main()
 {
     int wt[]={1,3,4,5};
     int val[]={1,4,5,7};
-    cout<<knapsack(wt,val,8,4);
+    int dp[8][4];
+    memset(dp,-1,sizeof(dp));
+    cout<<knapsack(wt,val,8,4,dp);
 
 }
